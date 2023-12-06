3b13186620d5fd48b9e08b7c01d1b87cd3e47ad7
Suheel Hussain <shussain@ttsiglobal.com>
2023-12-06 16:02:00 -0500

    Initial cut of my rest client

---
+++ src/restclient/__main__.py
@@ -0,0 +1,19 @@
+import logging
+# from restclient import cli
+
+
+
+def main():
+    print('Running main')
+#     logging.basicConfig(
+#         filename='run.log',
+#         format='%(asctime)s - %(levelname)s - %(message)s',
+#         level=logging.INFO,
+#     )
+    # creaete_logger()
+#     cli.execute()
+
+
+if __name__ == '__main__':
+    main()
+
